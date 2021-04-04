776f05b06a76241f870f3fd8fce6b5fec3659831
Katrina M <merca3@inbox.lv>
2021-04-04 14:52:59 +0300

    updated

--- Lots_Of_Tasks_HTML_01.04.21/js/main2.js
+++ Lots_Of_Tasks_HTML_01.04.21/js/main2.js
@@ -8,4 +8,13 @@
 
 function getFormvalue(submitEvent) {
     submitEvent.preventDefault();
+    const formElements = document.getElementById('form1');
+    // for (let i = 0; i < formElements.length; i++) {
+    //     console.log(formElements[i].value);
+    // }
+    for (let element of formElements) {
+        if (element.value !== 'Submit') {
+            console.log(element.value)
+        }
+    }
 }
--- Lots_Of_Tasks_HTML_01.04.21/js/main6.js
+++ Lots_Of_Tasks_HTML_01.04.21/js/main6.js
@@ -1,3 +1,33 @@
 function changeCell() {
+    const newCellTextInput = document.getElementById('new-cell-text');
+    const newCellText = newCellTextInput.value;
+    if (newCellText === '') {
+        alert('Please fill in the new cell text!');
+        return false;
+    }
+    findCell(newCellText);
+    newCellTextInput.value = '';
+}
 
+function findCell(newText) {
+    const rowNr = document.getElementById('choose-row').value;
+    const colNr = document.getElementById('choose-column').value;
+
+    if (rowNr == '1') {
+        if (colNr == '11') {
+            let cell = document.getElementById('sampleTable').rows[0].cells;
+            cell[0].innerHTML = `${newText}`;
+        } else {
+            cell = document.getElementById('sampleTable').rows[0].cells;
+            cell[1].innerHTML = `${newText}`;
+        }
+    } else {
+        if (colNr == '11') {
+            let cell = document.getElementById('sampleTable').rows[1].cells;
+            cell[0].innerHTML = `${newText}`;
+        } else {
+            cell = document.getElementById('sampleTable').rows[1].cells;
+            cell[1].innerHTML = `${newText}`;
+        }
+    }
 }
